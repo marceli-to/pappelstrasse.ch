1b2e8cd618122d5873df93ca024643c3b5b774ca
Marcel Stadelmann <m@marceli.to>
2025-11-20 21:49:43 +0100

    installs webfonts

--- resources/views/components/layout/body.blade.php
+++ resources/views/components/layout/body.blade.php
@@ -1,3 +1,3 @@
-<body class="antialiased flex flex-col min-h-screen">
+<body class="antialiased font-bebas-neue-pro font-medium flex flex-col min-h-screen">
   {{ $slot }}
 </body>
--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1,10 +1,28 @@
 @extends('app')
 @section('content')
 
-  <div class="bg-forest">
+  <div class="bg-forest text-white">
+
     <x-layout.inner>
-      <h1>Pappelstrasse Dietlikon</h1>
+
+      <h1 class="text-3xl">
+        Wohnen in Dietlikon
+      </h1>
+
+      <h2>
+        Erstvermietung
+      </h2>
+
+      <p>
+        An der Pappelstrasse 2/4 in Dietlikon entsteht per Herbst 2026 ein attraktives Neubauprojekt mit modernen 1.5- bis 3.5-Zimmerwohnungen.
+      </p>
+
+      <p>
+        <strong class="text-dew">Interessiert?</strong><br>Gerne senden wir Ihnen weitere Informationen zu, sobald die Vermietung startet. Bitte füllen Sie bei Interesse das Kontaktformular aus.
+      </p>
+    
     </x-layout.inner>
+
   </div>
   
   <div class="bg-dew">
