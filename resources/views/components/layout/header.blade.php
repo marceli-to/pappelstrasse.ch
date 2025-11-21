6a6b00d8e8f8d9d12d7f1e3556f0cabc2bb8ab03
Marcel Stadelmann <m@marceli.to>
2025-11-21 15:29:49 +0100

    wip

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,8 +1,8 @@
 <header class="w-full relative">
-  <x-media.visual :image="'pappelstrasse-dietlikon'" class="aspect-[16/9] md:aspect-[16/7] object-cover" />
+  <x-media.visual :image="'pappelstrasse-dietlikon'" class="aspect-[4/3] md:aspect-[16/9] lg:aspect-[16/8] object-cover" />
   <div class="absolute inset-0 w-full h-full">
     <x-layout.inner class="h-full relative">
-      <x-icons.logo class="w-100 md:w-130 lg:w-160 xl:w-176 absolute top-10 md:top-20 lg:top-40 left-10 md:left-20 lg:left-30" />
+      <x-icons.logo class="w-90 md:w-130 lg:w-180 h-auto absolute top-10 md:top-20 lg:top-40 left-15 md:left-20 lg:left-30" />
       <x-icons.symbol class="w-auto h-140 md:h-[16rem] lg:h-[20rem] xl:h-[25rem] absolute bottom-0 right-60 md:right-130 lg:right-180 xl:right-100" />
     </x-layout.inner>
   </div>
--- resources/views/components/layout/inner.blade.php
+++ resources/views/components/layout/inner.blade.php
@@ -1,3 +1,3 @@
-<div class="max-w-6xl mx-auto pt-10 md:pt-20 lg:pt-30 pb-15 md:pb-25 lg:pb-35 px-10 md:px-20 lg:px-30 {{ $class ?? '' }}">
+<div class="max-w-6xl mx-auto pt-10 md:pt-20 lg:pt-30 pb-15 md:pb-25 lg:pb-35 px-15 md:px-20 lg:px-30 {{ $class ?? '' }}">
   {{ $slot }}
 </div>
