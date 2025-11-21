9b20ab88fdf4fc6ae89ff53b2373f0e31ba92ed5
Marcel Stadelmann <m@marceli.to>
2025-11-21 15:23:49 +0100

    wip

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,5 +1,5 @@
 <header class="w-full relative">
-  <x-media.visual :image="'pappelstrasse-dietlikon'" class="aspect-[16/9] md:aspect-[16/7] xl:aspect-[16/6] object-cover" />
+  <x-media.visual :image="'pappelstrasse-dietlikon'" class="aspect-[16/9] md:aspect-[16/7] object-cover" />
   <div class="absolute inset-0 w-full h-full">
     <x-layout.inner class="h-full relative">
       <x-icons.logo class="w-100 md:w-130 lg:w-160 absolute top-10 md:top-20 lg:top-40 left-10 md:left-20 lg:left-30" />
