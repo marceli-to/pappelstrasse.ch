99e1669d71259ac6d91959e068fea351e11d57a1
Marcel Stadelmann <m@marceli.to>
2025-12-03 10:17:29 +0100

    adds meta tags

--- resources/views/components/layout/head.blade.php
+++ resources/views/components/layout/head.blade.php
@@ -8,5 +8,16 @@
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 <meta name="apple-mobile-web-app-title" content="Pappelstrasse" />
 <link rel="manifest" href="/site.webmanifest" />
+<meta property="og:title" content="{{ config('app.name', 'Pappelstrasse') }}" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ url()->current() }}" />
+<meta property="og:image" content="{{ asset('opengraph.jpg') }}" />
+<meta property="og:description" content="Erstvermietung: An der Pappelstrasse 2/4 in Dietlikon entsteht per Herbst 2026 ein attraktives Neubauprojekt mit modernen 1.5- bis 3.5-Zimmerwohnungen." />
+<meta property="og:site_name" content="{{ config('app.name', 'Pappelstrasse') }}" />
+<meta property="og:locale" content="de_CH" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="{{ config('app.name', 'Pappelstrasse') }}" />
+<meta name="twitter:description" content="Erstvermietung: An der Pappelstrasse 2/4 in Dietlikon entsteht per Herbst 2026 ein attraktives Neubauprojekt mit modernen 1.5- bis 3.5-Zimmerwohnungen." />
+<meta name="twitter:image" content="{{ asset('opengraph.jpg') }}" />
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
